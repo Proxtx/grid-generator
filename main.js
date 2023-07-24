0bb3988a03769df5c5580c9c0fb0b0aee432a31e
gopro54122@gmail.com <gopro54122@gmail.com>
2023-07-23 21:45:21 -0400

    small animation

--- main.js
+++ main.js
@@ -7,13 +7,24 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
-
-let renderer = new GridRenderer(ctx, new Grid({
+let g = new Grid({
   renderLines: false
-}))
+});
+let renderer = new GridRenderer(ctx, g)
 let renderer2 = new GridRenderer(ctx, new Grid({
   renderLines: false,
   rotation: 0
 }))
 renderer.render();
-renderer2.render();
+
+for(; g.options.rotation >= 0; g.options.rotation-=0.1){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  g.options.translateX-=0.1;
+  g.options.translateY -= 0.1;
+  g.options.cellWidth += 0.1;
+  g.options.cellHeight += 0.1
+  g.options.width += 0.01;
+  renderer.render();
+  await new Promise(r=> setTimeout(r, 5))
+}
+//renderer2.render();
